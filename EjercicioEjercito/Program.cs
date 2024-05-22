41506e6928e9a0c9c14986e67ea605c6a264c83e
maria-programa <mlozanoovejero314@gmail.com>
2024-05-22 11:08:27 +0200

    He añadido un segundo programa :)

--- EjercicioEjercito/Program.cs
+++ EjercicioEjercito/Program.cs
@@ -3,29 +3,106 @@ using EjercicioEjercito;
 using System.Runtime.CompilerServices;
 
 //-OPCION1
-IFabricaElementos MiFabrica = new Fabrica();
-IColeccionable MiEjercito = new Coleccion();
+//IFabricaElementos MiFabrica = new Fabrica();
+//IColeccionable MiEjercito = new Coleccion();
 
 
-ICaracteristicas TanqueSombra = MiFabrica.DameElemento(TipoElementos.Tanque);
-TanqueSombra = new SuperPiloto(new Desierto(new SuperBomba(TanqueSombra)));
+//ICaracteristicas TanqueSombra = MiFabrica.DameElemento(TipoElementos.Tanque);
+//TanqueSombra = new SuperPiloto(new Desierto(new SuperBomba(TanqueSombra)));
 
-MiEjercito.Add(MiFabrica.DameElemento(TipoElementos.TransporteMX));
-MiEjercito.Add(MiFabrica.DameElemento(TipoElementos.Sanitario));
-MiEjercito.Add(MiFabrica.DameElemento(TipoElementos.Ametrallador));
-MiEjercito.Add(MiFabrica.DameElemento(TipoElementos.TransporteMX));
-MiEjercito.Add(TanqueSombra);
+//MiEjercito.Add(MiFabrica.DameElemento(TipoElementos.TransporteMX));
+//MiEjercito.Add(MiFabrica.DameElemento(TipoElementos.Sanitario));
+//MiEjercito.Add(MiFabrica.DameElemento(TipoElementos.Ametrallador));
+//MiEjercito.Add(MiFabrica.DameElemento(TipoElementos.TransporteMX));
+//MiEjercito.Add(TanqueSombra);
 
-Console.WriteLine($"El ejercito tiene {MiEjercito.ElementosTotales()} elementos");
-Console.WriteLine($"Su potencia de fuego es de {MiEjercito.PotenciaFuegoTotal()}");
-Console.WriteLine($"Su blindaje es de {MiEjercito.BlindajeTotal()}");
-Console.WriteLine($"La velocidad total es {MiEjercito.VelocidadTotal()}");
-Console.WriteLine($"Se ha gastado en total {MiEjercito.PrecioTotal()} euros");
-Console.WriteLine($"Tiene una Capacidad Militar de {MiEjercito.CapacidadMilitar()}");
-
-
+//Console.WriteLine($"El ejercito tiene {MiEjercito.ElementosTotales()} elementos");
+//Console.WriteLine($"Su potencia de fuego es de {MiEjercito.PotenciaFuegoTotal()}");
+//Console.WriteLine($"Su blindaje es de {MiEjercito.BlindajeTotal()}");
+//Console.WriteLine($"La velocidad total es {MiEjercito.VelocidadTotal()}");
+//Console.WriteLine($"Se ha gastado en total {MiEjercito.PrecioTotal()} euros");
+//Console.WriteLine($"Tiene una Capacidad Militar de {MiEjercito.CapacidadMilitar()}");
 
 
+//-OPCION2
+
+IFabricaElementos MiFabrica02 = new Fabrica();
+IColeccionable MiEjercito02 = new Coleccion();
+
+string[] opciones = { "MX-7899", "Tanque de ataque Sombras-VB98", "TAXIN-66", "Infanteria Basica", "Ametrallador", "Sanitario", "Cañon antiaereo", "Torpedero Movil", "Cañon", "¿Quiere saber los datos importantes hasta ahora?" };
+int opcion;
+
+Console.WriteLine("Buenos días general. Configure su ejercito. Por favor, seleccione las opciones que guste. \n");
+
+
+for (int i = 0; i < opciones.Length; i++)
+{
+    Console.WriteLine(i + 1 + ". " + opciones[i]);
+}
+Console.WriteLine("0. Salir");
+do
+{
+    opcion = Int32.Parse(Console.ReadLine());
+    switch (opcion)
+    {
+        case 0:
+            Console.WriteLine("Adios");
+            Environment.Exit(0);
+            break;
+        case 1:
+            Escribe(TipoElementos.TransporteMX);
+            Console.WriteLine("MX-7899");
+            break;
+        case 2:
+            Escribe(TipoElementos.Tanque);
+            Console.WriteLine("Tanque de ataque Sombras-VB98");
+            break;
+        case 3:
+            Escribe(TipoElementos.TransporteRapido);
+            Console.WriteLine("TAXIN-66");
+            break;
+        case 4:
+            Escribe(TipoElementos.Infanteria);
+            Console.WriteLine("Infanteria Basica");
+            break;
+        case 5:
+            Escribe(TipoElementos.Ametrallador);
+            Console.WriteLine("Ametrallador");
+            break;
+        case 6:
+            Escribe(TipoElementos.Sanitario);
+            Console.WriteLine("Sanitario");
+            break;
+        case 7:
+            Escribe(TipoElementos.CanonAntiaereo);
+            Console.WriteLine("Cañon antiaereo");
+            break;
+        case 8:
+            Escribe(TipoElementos.Torpedero);
+            Console.WriteLine("Torpedero Movil");
+            break;
+        case 9:
+            Escribe(TipoElementos.Canon);
+            Console.WriteLine("Cañon");
+            break;
+        case 10:
+            Datos();
+            break;
+    }
+} while (opcion != 0);
+
+
+void Escribe(TipoElementos tipo)
+{
+    ICaracteristicas MiUnidad = MiFabrica02.DameElemento(tipo);
+    MiEjercito02.Add(MiUnidad);
+}
+
+void Datos()
+{
+    Console.WriteLine($"El ejercito tiene {MiEjercito02.ElementosTotales()} elementos \nSu potencia de fuego es de {MiEjercito02.PotenciaFuegoTotal()} \nSu blindaje es de {MiEjercito02.BlindajeTotal()}\n" +
+        $"La velocidad total es {MiEjercito02.VelocidadTotal()} \nSe ha gastado en total {MiEjercito02.PrecioTotal()} euros \nTiene una Capacidad Militar de {MiEjercito02.CapacidadMilitar()}");
+}
 //-OPCION2
 //IFabricaElementos MiFabrica = new Fabrica();
 //IColeccionable MiEjercito = new Coleccion();
